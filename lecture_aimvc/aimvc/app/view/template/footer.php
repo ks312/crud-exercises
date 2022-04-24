1aa164935223bde002e852a1a7cb149491cdc97c
sinkoo <69867339+sinkoo0@users.noreply.github.com>
2022-04-24 16:16:48 +0800

    Add files via upload

---
+++ lecture_aimvc/aimvc/app/view/template/footer.php
@@ -0,0 +1,8 @@
+<script src="<?php echo APP_PATH; ?>/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dis/umd/popper.min.js"></script>
+<script src="<?php echo APP_PATH; ?>/js/bootstrap.min.js"></script>
+<script src="<?php echo APP_PATH; ?>/js/bootstrap.min.js"></script>
+<script src="<?php echo APP_PATH; ?>/js/jquery-3.6.0.min.js"></script>
+
+</html>
+</body>
